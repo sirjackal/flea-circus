a37ffa0d465131d1c4d48bc784dada69396818be
Jan Krepsky <jan.krepsky@invia.com>
2026-01-02 10:36:17 +0100

    Refactoring

--- main.go
+++ main.go
@@ -10,15 +10,16 @@ import (
 const gridSize = 30
 const ringBellsCount = 50
 const simulationsCount = 10000
+const workersCount = 128
 
 type GridSquare struct {
-	Row             uint
-	Col             uint
+	Row             int
+	Col             int
 	Fleas           []*Flea
 	AdjacentSquares []*GridSquare
 }
 
-func NewGridSquare(row, col uint) GridSquare {
+func NewGridSquare(row, col int) GridSquare {
 	return GridSquare{
 		Row: row,
 		Col: col,
@@ -58,8 +59,8 @@ func NewGrid() Grid {
 	var g Grid
 	g.Fleas = []*Flea{}
 
-	for row := uint(0); row < gridSize; row++ {
-		for col := uint(0); col < gridSize; col++ {
+	for row := 0; row < gridSize; row++ {
+		for col := 0; col < gridSize; col++ {
 			var square = NewGridSquare(row, col)
 			var flea = NewFlea(&square)
 			square.Fleas = []*Flea{&flea}
@@ -68,8 +69,8 @@ func NewGrid() Grid {
 		}
 	}
 
-	for row := uint(0); row < gridSize; row++ {
-		for col := uint(0); col < gridSize; col++ {
+	for row := 0; row < gridSize; row++ {
+		for col := 0; col < gridSize; col++ {
 			g.Squares[row][col].AdjacentSquares = getAdjacentSquares(&g, g.Squares[row][col])
 		}
 	}
@@ -78,8 +79,8 @@ func NewGrid() Grid {
 }
 
 func (g *Grid) Print(withAdjacentSquares bool) {
-	for row := uint(0); row < gridSize; row++ {
-		for col := uint(0); col < gridSize; col++ {
+	for row := 0; row < gridSize; row++ {
+		for col := 0; col < gridSize; col++ {
 			fmt.Printf("%v\n", g.Squares[row][col].ToString(withAdjacentSquares))
 		}
 	}
@@ -115,18 +116,18 @@ func getAdjacentSquares(grid *Grid, square *GridSquare) []*GridSquare {
 		var newCol = int(square.Col) + m.X
 
 		if newRow >= 0 && newRow < gridSize && newCol >= 0 && newCol < gridSize {
-			adjacentSquares = append(adjacentSquares, grid.Squares[uint(newRow)][uint(newCol)])
+			adjacentSquares = append(adjacentSquares, grid.Squares[newRow][newCol])
 		}
 	}
 
 	return adjacentSquares
 }
 
-func countUnoccupiedFields(g *Grid) uint {
-	count := uint(0)
+func countUnoccupiedFields(g *Grid) int {
+	count := 0
 
-	for row := uint(0); row < gridSize; row++ {
-		for col := uint(0); col < gridSize; col++ {
+	for row := 0; row < gridSize; row++ {
+		for col := 0; col < gridSize; col++ {
 			if len(g.Squares[row][col].Fleas) == 0 {
 				count++
 			}
@@ -166,8 +167,8 @@ func ringBell(g *Grid) {
 func checkFleasCount(g *Grid) {
 	count := 0
 
-	for row := uint(0); row < gridSize; row++ {
-		for col := uint(0); col < gridSize; col++ {
+	for row := 0; row < gridSize; row++ {
+		for col := 0; col < gridSize; col++ {
 			count += len(g.Squares[row][col].Fleas)
 		}
 	}
@@ -179,7 +180,7 @@ func checkFleasCount(g *Grid) {
 	}
 }
 
-func runSingleSimulation() uint {
+func runSingleSimulation() int {
 	var g = NewGrid()
 	//g.Print(true)
 
@@ -192,49 +193,49 @@ func runSingleSimulation() uint {
 	return countUnoccupiedFields(&g)
 }
 
-func worker(jobs <-chan uint, results chan<- uint) {
+func worker(jobs <-chan int, results chan<- int) {
 	for range jobs {
 		results <- runSingleSimulation()
 	}
 }
 
-func runParallelSimulation(simulationsCount uint) {
+func runParallelSimulation(simulationsCount int) {
 	var start = time.Now()
 
-	const workersCount = 128
-	jobs := make(chan uint, simulationsCount)
-	results := make(chan uint, simulationsCount)
+	jobs := make(chan int, simulationsCount)
+	results := make(chan int, simulationsCount)
 
-	for w := uint(1); w <= workersCount; w++ {
+	for w := 1; w <= workersCount; w++ {
 		go worker(jobs, results)
 	}
 
-	for j := uint(1); j <= simulationsCount; j++ {
+	for j := 1; j <= simulationsCount; j++ {
 		jobs <- j
 	}
 	close(jobs)
 
-	var sum uint
+	var sum int
 
-	for a := uint(1); a <= simulationsCount; a++ {
+	for a := 1; a <= simulationsCount; a++ {
 		sum += <-results
 	}
+	close(results)
 
-	var elapsedTime = time.Now().Sub(start)
-	fmt.Printf("Average count of unoccupied fields: %f\n", float32(sum)/float32(simulationsCount))
+	var elapsedTime = time.Since(start)
+	fmt.Printf("Average count of unoccupied fields: %.6f\n", float32(sum)/float32(simulationsCount))
 	fmt.Printf("Elapsed time: %s", elapsedTime)
 }
 
-func runSequentialSimulation(simulationsCount uint) {
+func runSequentialSimulation(simulationsCount int) {
 	var start = time.Now()
-	var sum uint
+	var sum int
 
-	for i := uint(1); i <= simulationsCount; i++ {
+	for i := 1; i <= simulationsCount; i++ {
 		sum += runSingleSimulation()
 	}
 
-	var elapsedTime = time.Now().Sub(start)
-	fmt.Printf("Average count of unoccupied fields: %f\n", float32(sum)/float32(simulationsCount))
+	var elapsedTime = time.Since(start)
+	fmt.Printf("Average count of unoccupied fields: %.6f\n", float32(sum)/float32(simulationsCount))
 	fmt.Printf("Elapsed time: %s", elapsedTime)
 }
 
